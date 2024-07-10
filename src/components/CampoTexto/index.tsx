53745253669d378eb7c58b1165dd7a53eb566305
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-07-10 10:45:15 -0300

    raw: Modifying the 'tsconfig.json' file to allow the creation of components without needing to import the 'React' module directly in each component file. Migrating the 'CampoTexto' component to TypeScript

---
+++ src/components/CampoTexto/index.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent } from 'react'
+
+import './CampoTexto.css'
+
+interface CampoTextoProps {
+  aoAlterado: (value: string) => void
+  label: string
+  valor: string
+  obrigatorio: boolean
+  placeholder: string
+}
+
+const CampoTexto = ({
+  aoAlterado,
+  label,
+  valor,
+  obrigatorio,
+  placeholder
+}: CampoTextoProps) => {
+  const aoDigitado = (evento: ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value)
+  }
+
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={obrigatorio}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
+
+export default CampoTexto
